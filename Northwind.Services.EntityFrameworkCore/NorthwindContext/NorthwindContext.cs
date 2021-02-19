15d32804806a6f754cc62bfa973dd1d098a76cd6
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-02-19 12:24:25 +0300

    Second task completed.

---
+++ Northwind.Services.EntityFrameworkCore/NorthwindContext/NorthwindContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Northwind.Services.EntityFrameworkCore.Entities;
+
+#nullable disable
+#pragma warning disable CA1062
+#pragma warning disable CS1591
+#pragma warning disable SA1600
+#pragma warning disable SA1601
+
+namespace Northwind.Services.EntityFrameworkCore.NorthwindContext
+{
+    public partial class NorthwindContext : DbContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NorthwindContext"/> class.
+        /// </summary>
+        public NorthwindContext()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NorthwindContext"/> class.
+        /// </summary>
+        /// <param name="options">DbContextOptions.</param>
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        /// <inheritdoc/>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("data source=(localdb)\\MSSQLLocalDB;Integrated Security=True;Database=Northwind;");
+            }
+        }
+
+        /// <inheritdoc/>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            this.OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
+#pragma warning restore SA1601
+#pragma warning restore SA1600
+#pragma warning restore CS1591
+#pragma warning restore CA1062
