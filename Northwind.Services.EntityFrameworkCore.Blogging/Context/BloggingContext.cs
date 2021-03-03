17b1ac03e48c89fda0ce32484f6ae0b3574857f9
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-03-03 12:29:18 +0300

    The 3rd point has been fulfilled.

---
+++ Northwind.Services.EntityFrameworkCore.Blogging/Context/BloggingContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Services.EntityFrameworkCore.Blogging.Context
+{
+    public class BloggingContext
+    {
+    }
+}
---
+++ Northwind.Services.EntityFrameworkCore.Blogging/Entities/BlogArticle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Services.EntityFrameworkCore.Blogging.Entities
+{
+    public class BlogArticle
+    {
+    }
+}
