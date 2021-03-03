64b27bcde3d7fa18560304719680007a12ffbb01
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2021-03-03 18:29:40 +0300

    Added BloggingContext.cs context.

--- Northwind.Services/Blogging/BlogArticle.cs
+++ Northwind.Services/Blogging/BlogArticle.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Northwind.Services.Blogging
 {
     public class BlogArticle
     {
-        [Key]
-        [Column("BlogArticleID")]
         public int BlogArticleId { get; set; }
 
-        [StringLength(50)]
         public string Title { get; set; }
 
-        [StringLength(4000)]
         public string Body { get; set; }
 
-        [Column(TypeName = "date")]
         public DateTime PublicationDate { get; set; }
 
-        [Column("EmployeeID")]
         public int EmployeeId { get; set; }
     }
 }
